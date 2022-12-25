c5fcbc2f208c6d3dd994e171d8979397679fce3b
André Sortland <arsortland@gmail.com>
2022-12-25 14:20:43 +0100

    second commit

---
+++ src/dom.js
@@ -0,0 +1,4 @@
+export const body = document.getElementById("body");
+export const header = document.createElement("div");
+export const sidebar = document.createElement("div");
+export const main = document.createElement("div");
--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import "./style.css";
 import "./logic.js";
-
-const testh1 = document.createElement("h1");
-testh1.textContent = "Klæssik Marlin";
-
-const testbody = document.querySelector("body");
-testbody.appendChild(testh1);
