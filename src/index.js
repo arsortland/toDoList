dc5d6fac83510f7f489f1538c7e18fc5fcadcfb6
André Sortland <arsortland@gmail.com>
2022-12-25 13:46:13 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import "./logic.js";
+
+const testh1 = document.createElement("h1");
+testh1.textContent = "Klæssik Marlin";
+
+const testbody = document.querySelector("body");
+testbody.appendChild(testh1);
---
+++ src/logic.js
@@ -0,0 +1,3 @@
+const toDoFactory = (name) => {
+  return { name };
+};
